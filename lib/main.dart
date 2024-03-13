92b6c9bf7f191d4c39a8beec7897ea728cf5c033
Soli Hosseini <s.hosseini.309@gmail.com>
2024-03-13 14:34:49 +0330

    #first release #simple functionality

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class QuoteGeneratorApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Quote Generator',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
